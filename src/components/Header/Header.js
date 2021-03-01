189e90e60beff92e69310f26dec821c0ea544d75
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-03-01 13:42:54 +0100

    added logo and nav icon to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import { HiMenu } from "react-icons/hi"
+import styles from "./Header.module.scss"
 
-const Header = () => <h1>header</h1>
+const Header = () => {
+  const { logo } = useStaticQuery(query)
+  return (
+    <header>
+      <section className={styles.container}>
+        <Link to="/">
+          <Img fixed={logo.childImageSharp.fixed} />
+        </Link>
+        <div className={styles.icon}>
+          <HiMenu />
+        </div>
+      </section>
+    </header>
+  )
+}
+
+const query = graphql`
+  query Logo {
+    logo: file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fixed(width: 200, quality: 90) {
+          ...GatsbyImageSharpFixed_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default Header
